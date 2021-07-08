19113392d8a2abdb6bb58c2623fdf3327c8dc5b7
unloccode <unloccode@gmail.com>
2021-07-08 22:12:18 +0300

    Made changes to App.js and Added a admin and moderator pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-//import './App.css';
 import React from 'react';
+import {BroserRouter as Switch, Route, Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import AuthService from './services/AuthService.js';
+
+import Login from './components/Login.js';
+import Register from './components/Register.js';
+import Homer from './components/Homer.js';
+import Profile from './components/Profile.js';
+import UserBoard from './components/UserBoard.js';
+
 
 class App extends React.Component{
   render(){
---
+++ src/components/AdminBoard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import UserService from '../services/UserService.js';
+
+export default class AdminUser extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            content: ""
+        };
+    }
+    componentDidMount(){
+        UserService.getAdminBoard().then(
+            response=>{
+                this.setState({
+                    content: response.data
+                });
+            },
+            error=>{
+                this.setState({
+                    content: (error.response && error.response.data && error.response.data.message) ||
+                    error.message ||
+                    error.toString()
+                });
+            }
+        );
+    }
+    render(){
+        return(
+            <div className="container">
+                <header className="jumbotron">
+                    <h3>{this.state.content}</h3>
+                </header>
+            </div>
+        );
+    }
+}
