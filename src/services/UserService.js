d15365c830bb4487a7217de0b9ef6320d31318bc
unloccode <unloccode@gmail.com>
2021-07-08 08:40:18 +0300

    Updated AuthHeaders and UserService

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,1 +1,22 @@
 //Data service
+import axios from 'axios';
+import AuthHeader from '../services/AuthHeader';
+
+const API_URL = 'http://localhost:8080/api/test/';
+
+class UserService{
+    getPublicContent(){
+        return axios . get(API_URL + 'all');
+    }
+    getUserBoard(){
+        return axios . get(API_URL + 'user', {headers: AuthHeader()});        
+    }
+    getModeratorBoard(){
+        return axios . get(API_URL + 'mod', {headers: AuthHeader()});
+    }
+    getAdminBoard(){
+        return axios.get(API_URL + 'admin', {headers: AuthHeader()});
+    }
+}
+
+export default new UserService();
