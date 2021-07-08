550a1b5090cebff6650062bf831d8d9fa2c072db
unloccode <unloccode@gmail.com>
2021-07-08 08:29:15 +0300

    Update the AuthService

---
+++ src/services/AuthService.js
@@ -0,0 +1,36 @@
+//Authentication service
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/auth";
+
+class AuthService{
+    //login
+    login(username, password){
+        return axios . post(API_URL + "signin", {
+            username,
+            password
+        }).then(response=>{
+            if(response.data.accessToken){
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+        })
+    }
+    //logout
+    logout(){
+        localStorage.removeItem("user");
+    }
+    //register_user
+    register(username, email, password){
+        return axios . post(API_URL + "signup", {
+            username,
+            email,
+            password
+        });
+    }
+    //get_current_user
+    getCurrentUser(){
+        return JSON.parse(localStorage.getItem('user'));
+    }
+}
+
+export default new AuthService();
